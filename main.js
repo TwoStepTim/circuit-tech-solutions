61b615f3d43f4cb4948b711c791487c0da955ab2
TwoStepTim <tjmims33@gmail.com>
2023-11-27 10:23:32 -0500

    Init Code

---
+++ main.js
@@ -0,0 +1,23 @@
+const roundedCursor = document.querySelector('.rounded');
+
+const moveCursor = (event) => {
+    const { clientX, clientY } = event;
+
+    // Adjust for scroll position
+    const scrolledY = clientY + window.scrollY;
+
+    // Update the rounded cursor position
+    const roundedX = clientX - roundedCursor.offsetWidth / 2.6;
+    const roundedY = scrolledY - roundedCursor.offsetHeight / 2.7;
+
+    // Apply the transition for smooth movement
+    roundedCursor.style.transition = 'transform 0.5s ease-out';
+    roundedCursor.style.transform = `translate3d(${roundedX}px, ${roundedY}px, 0)`;
+};
+
+
+const resetCursor = () => {
+    // Remove the transition when the cursor is not moving
+    roundedCursor.style.transition = 'none';
+};
+
