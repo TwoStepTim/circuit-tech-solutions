2b7f94ad7f4c0f0b9ee254a82b6acaa56dc678d5
TwoStepTim <tjmims33@gmail.com>
2023-11-27 13:48:39 -0500

    Update fixes

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const moveCursor = (event) => {
     const scrolledY = clientY + window.scrollY;
 
     // Update the rounded cursor position
-    const roundedX = clientX - roundedCursor.offsetWidth / 2.6;
-    const roundedY = scrolledY - roundedCursor.offsetHeight / 2.7;
+    const roundedX = clientX - roundedCursor.offsetWidth /  2.6;
+    const roundedY = scrolledY - roundedCursor.offsetHeight /  2.7;
 
     // Apply the transition for smooth movement
     roundedCursor.style.transition = 'transform 0.5s ease-out';
@@ -21,3 +21,5 @@ const resetCursor = () => {
     roundedCursor.style.transition = 'none';
 };
 
+window.addEventListener('mousemove', moveCursor);
+window.addEventListener('mouseleave', resetCursor);
